246f47b7144dcea93d15e1c4f060a79b4cc3bae5
an5dy <846562014@qq.com>
2022-04-20 16:27:06 +0800

    服务注册

--- main/main.go
+++ main/main.go
@@ -11,8 +11,22 @@ import (
 	"github.com/An5dy/go-rpc/server"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 // startServer 使用了信道 addr，确保服务端端口监听成功，客户端再发起请求
 func startServer(addr chan<- string) {
+	// 注册 Foo 服务
+	var foo Foo
+	if err := server.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
@@ -39,12 +53,12 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
